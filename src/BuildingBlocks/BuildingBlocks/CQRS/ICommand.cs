3fbefc0c151b4dcdc6f85f5306b63ae96db2b101
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-05-21 09:31:31 +0300

    BuildingBlocks --> CQRS --> ICommand & ICommandHandler: Updated the interfaces (implement the ICommand & ICommandHandler in the void interfaces)

--- src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -2,5 +2,5 @@
 
 namespace BuildingBlocks.CQRS;
 
-public interface ICommand:IRequest<Unit> { } // The Unit is a type from the MediatR that represent a void type
+public interface ICommand:ICommand<Unit> { } // The Unit is a type from the MediatR that represent a void type
 public interface ICommand<out TResponse> : IRequest<TResponse> { }
