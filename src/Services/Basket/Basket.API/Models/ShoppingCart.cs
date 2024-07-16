06c0afc22a01a47341019459df0bf30a3193f9de
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-07-16 14:41:39 +0300

    Basket.API: Added the models.

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public string Username { get; set; } = default!;
+    public IEnumerable<ShoppingCartItem> Items { get; set; } = default!;
+    public decimal TotalPrice => Items == null 
+        ? 0m 
+        : Items.Sum(i => i.Price * i.Quantity);
+
+    public ShoppingCart(string username)
+    {
+        Username = username;
+    }
+
+    public ShoppingCart()
+    {
+        
+    }
+}
