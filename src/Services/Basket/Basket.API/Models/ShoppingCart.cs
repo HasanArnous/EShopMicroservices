21381bbc445a2c8f6dbccb8266dc07ef909fd7f0
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-07-25 10:30:41 +0300

    Basket --> Models --> ShoppingCart: Fixed the type of the items (IEnumerable<> -> List<>), it was causing issues and throwing exception in the Marten library.

--- src/Services/Basket/Basket.API/Models/ShoppingCart.cs
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -3,7 +3,7 @@
 public class ShoppingCart
 {
     public string Username { get; set; } = default!;
-    public IEnumerable<ShoppingCartItem> Items { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = default!;
     public decimal TotalPrice => Items == null 
         ? 0m 
         : Items.Sum(i => i.Price * i.Quantity);
