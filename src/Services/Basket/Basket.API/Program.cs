67d49b419a2917f1866e479eeaff8e3889256057
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-07-30 09:53:31 +0300

    Basket --> Program.cs: Configured Redis cache server, and registered the Cached basket repository decorator with the 'Scrutor' library.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,21 @@
 using BuildingBlocks.Exceptions.Handler;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//? Registering the decoration with Scrutor
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+//? Registering the decoration manually
+//builder.Services.AddScoped<IBasketRepository>(provider =>
+//{
+//    var basketRepo = provider.GetRequiredService<IBasketRepository>();
+//    return new CachedBasketRepository(basketRepo, provider.GetRequiredService<IDistributedCache>());
+//});
 
 builder.Services.AddMediatR(config =>
 {
