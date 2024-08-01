a0223352fb40809a2b02bf12061b185c809a338f
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-08-01 17:14:51 +0300

    Discount.gRPC: - Removed the sample files - Added the Coupon model + discount.proto - Configured Server Reflection for development environment.

---
+++ src/Services/Discount/Discount.gRPC/Models/Coupon.cs
@@ -0,0 +1,9 @@
+﻿namespace Discount.gRPC.Models;
+
+public class Coupon
+{
+    public int Id { get; set; }
+    public Guid ProductId { get; set; }
+    public string Description { get; set; } = default!;
+    public double Amount { get; set; }
+}
