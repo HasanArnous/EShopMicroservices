366662a7779d95532e35bef08f0485474c18af6b
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-08-02 20:28:38 +0300

    Discount: - Added the DiscountService - Map the Discount gRPC Service

--- src/Services/Discount/Discount.gRPC/Program.cs
+++ src/Services/Discount/Discount.gRPC/Program.cs
@@ -1,5 +1,6 @@
 //using Discount.gRPC.Services;
 
+using Discount.gRPC.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +14,8 @@ if (builder.Environment.IsDevelopment())
     app.MapGrpcReflectionService();
 
 // Configure the HTTP request pipeline.
+
+app.MapGrpcService<DiscountService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
