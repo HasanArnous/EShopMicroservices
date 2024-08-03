fe38ceec4dada7de0e61210f51ccca96393a64df
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-08-03 12:29:48 +0300

    Discount --> Services --> DiscountService: Overrode the Discount Service Methods.

--- src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
+++ src/Services/Discount/Discount.gRPC/Services/DiscountService.cs
@@ -1,26 +1,66 @@
-﻿using Grpc.Core;
+﻿using Discount.gRPC.Data;
+using Discount.gRPC.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.gRPC.Services;
 
-public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService
+	(DiscountDbContext db, ILogger<DiscountService> logger)
+	: DiscountProtoService.DiscountProtoServiceBase
 {
-	public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+	public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
 	{
-		return base.GetDiscount(request, context);
+		if(Guid.TryParse(request.ProductId, out Guid productId))
+			throw new RpcException(status: new Status(StatusCode.InvalidArgument, "Invalid Product ID"));
+		var coupon = await db.Coupones.FirstOrDefaultAsync(c => c.ProductId == productId);
+		if(coupon == null) 
+			coupon = new Coupon {Id = -1, ProductId = productId, Description = "No Coupone for this Product!", Amount = 0 };
+		logger.LogInformation("Coupone for {ProductId}, Amount: {CuoponAmount}", request.ProductId, coupon.Amount);
+		var couponModel = coupon.Adapt<CouponModel>();
+		return couponModel;
 	}
 
-	public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+	public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
 	{
-		return base.CreateDiscount(request, context);
+		var coupon = request.Coupon.Adapt<Coupon>();
+		if (coupon == null)
+			throw new RpcException(status: new Status(StatusCode.InvalidArgument, "Invalid Coupon detials"));
+		db.Coupones.Add(coupon);
+		await db.SaveChangesAsync();
+		logger.LogInformation("Coupons - Create - Success - Coupon Id: {CouponId}, ProductId: {ProductId}", coupon.Id, coupon.ProductId);
+		return coupon.Adapt<CouponModel>();
 	}
 
-	public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+	public async override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
 	{
-		return base.UpdateDiscount(request, context);
+		var coupon = request.Coupon.Adapt<Coupon>();
+		if (coupon == null)
+			throw new RpcException(status: new Status(StatusCode.InvalidArgument, "Invalid Coupon detials"));
+		if (await db.Coupones.CountAsync(c => c.Id == request.Coupon.Id) == 0)
+		{
+			logger.LogWarning("Coupones - Update - NOT FOUND - Coupone Id: {CouponId}", request.Coupon.Id);
+			throw new RpcException(new Status(StatusCode.NotFound, "No Discount Found!"));
+		}
+		db.Coupones.Update(coupon);
+		await db.SaveChangesAsync();
+		logger.LogInformation("Coupons - Update - Success - Coupon Id: {CouponId}, ProductId: {ProductId}", coupon.Id, coupon.ProductId);
+		return coupon.Adapt<CouponModel>();
 	}
 
-	public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+	public async override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
 	{
-		return base.DeleteDiscount(request, context);
+		if(Guid.TryParse(request.ProductId, out Guid productId))
+			throw new RpcException(status: new Status(StatusCode.InvalidArgument, "Invalid Product ID"));
+		var coupon = await db.Coupones.FirstOrDefaultAsync(c => c.ProductId == productId);
+		if (coupon == null)
+		{
+			logger.LogWarning("Coupones - DELETE - NOT FOUND - Coupone Id: {CouponId}", productId);
+			throw new RpcException(new Status(StatusCode.NotFound, "No Discount Found!"));
+		}
+		db.Coupones.Remove(coupon);
+		await db.SaveChangesAsync();
+		return new DeleteDiscountResponse { IsSuccess = true };
 	}
 }
