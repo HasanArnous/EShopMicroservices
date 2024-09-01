56f920e8dc1c02c7f869a2f761771b2b3d8b4dac
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-09-01 17:56:12 +0300

    Ordering: Enabled Auto-Migration + Seed Data (Only in the Development Environment)...

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Core.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,5 +13,9 @@ builder.Services
 var app = builder.Build();
 
 app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+    await app.InitializeDbAsync();
+
 
 app.Run();
