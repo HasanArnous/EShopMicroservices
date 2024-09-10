fc89401e8e2461f393a7f0a22cb7f5be7727a96f
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-09-10 17:21:47 +0300

    Ordering --> Core.Application --> DependencyInjection: Registered Mediator behaviors: 1- Validation 2- Logging

--- src/Services/Ordering/Ordering.Core.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Core.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Ordering.Core.Application;
@@ -8,7 +9,11 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         // Register the services into the IoC container...
-        services.AddMediatR(config => config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        services.AddMediatR(config => {
+            config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
 
         return services;
     }
