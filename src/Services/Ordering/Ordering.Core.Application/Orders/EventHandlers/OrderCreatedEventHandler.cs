acafdb3e65ca5094a37457fbbba9267311ff64af
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-09-10 17:22:39 +0300

    Ordering --> Core.Application: Added Event handlers for the Order aggregate domain events (currently only for logging)

---
+++ src/Services/Ordering/Ordering.Core.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Core.Application.Orders.EventHandlers;
+
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        // Currently will handle only logging, but the future integration event will published.
+        logger.LogInformation("Domain Event Handled: {DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Core.Application/Orders/EventHandlers/OrderUpdatedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Ordering.Core.Application.Orders.EventHandlers;
+
+public class OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger) : INotificationHandler<OrderUpdatedEvent>
+{
+    public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        // Currently will handle only logging, but the future integration event will published.
+        logger.LogInformation("Domain Event Handled: {DomainEvent}", notification.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
