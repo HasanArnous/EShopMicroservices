0b280632b1da8a29828eed225776200e9bb166fc
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-09-11 17:13:37 +0300

    Ordering --> API: Added the required dependencies + Configure the dependency injection...

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -5,13 +5,14 @@ public static class DependencyInjection
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
         // Register the services into the IoC container
-
+        services.AddCarter();
         return services;
     }
 
     public static WebApplication UseApiServices(this WebApplication app) 
     {
         // Use all the required services...
+        app.MapCarter();
         return app;
     }
 }
