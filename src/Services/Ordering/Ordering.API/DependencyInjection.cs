cf3c3fee2d806bef2e61eb56e76de260d4da208e
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-09-13 23:10:09 +0300

    Ordering --> API --> DependencyInjection: Using the custom exception handler...

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,10 +1,14 @@
+﻿using BuildingBlocks.Exceptions.Handler;
 ﻿namespace Ordering.API;
+
+namespace Ordering.API;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
         // Register the services into the IoC container
+        services.AddExceptionHandler<CustomExceptionHandler>();
         services.AddCarter();
         return services;
     }
@@ -12,6 +16,7 @@ public static class DependencyInjection
     public static WebApplication UseApiServices(this WebApplication app) 
     {
         // Use all the required services...
+        app.UseExceptionHandler(options => { });
         app.MapCarter();
         return app;
     }
