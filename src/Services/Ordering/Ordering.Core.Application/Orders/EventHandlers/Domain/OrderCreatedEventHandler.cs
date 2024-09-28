b866f92cf194a3009e36b607aae3e68c75b81ae9
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-09-28 12:15:58 +0300

    Ordering.Core.Application --> Orders --> EventHandlers --> Domain --> OrderCreatedEventHandler: Published an event to notify the Order Fullfilment services about the new Order (Need Attention regarding making it a typed event)

--- src/Services/Ordering/Ordering.Core.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Core.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,11 +1,19 @@
 ﻿namespace Ordering.Core.Application.Orders.EventHandlers;
 
-public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+public class OrderCreatedEventHandler(IPublisher publisher, ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
 {
-	public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+	public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
 	{
-		// Currently will handle only logging, but the future integration event will published.
-		logger.LogInformation("Domain Event Handled: {DomainEvent}", notification.GetType().Name);
-		return Task.CompletedTask;
+		logger.LogInformation("Domain Event Handled: {DomainEvent}", domainEvent.GetType().Name);
+		// Publishing an Integration Event to notify other services that there is a new Order was created, 
+		// Order Fullfilment processes like Payment, Shipping, Notification.... will handle this event and act upone it.
+
+		//? NOTE AS A PROBLEM (NEED TO CHECK FROM THE COURSE CREATOR)
+		// Shouldn't we have to create a class/record (typed event) for this integration event
+		// as the same of what we did in the BasketCheckoutEvent so when we add a consumer class
+		// we can specify the type of the event and get the details of the object?????
+		// Currently, we are publishing the OrderDTO object instead of a specific type for the integration event.....
+		var orderCreatedIntegrationEvent = domainEvent.order.ToDto();
+		await publisher.Publish(orderCreatedIntegrationEvent, cancellationToken);
 	}
 }
