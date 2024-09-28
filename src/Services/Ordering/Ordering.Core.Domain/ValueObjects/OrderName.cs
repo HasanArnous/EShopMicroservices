ac3977caf6b78657118cf60ce1b1fb316efa948d
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-09-28 14:06:26 +0300

    Ordering.Core.Domain.ValueObjects --> OrderName: Comment out the validation rule of the length on the order name.

--- src/Services/Ordering/Ordering.Core.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Core.Domain/ValueObjects/OrderName.cs
@@ -11,7 +11,8 @@ public record OrderName
     public static OrderName Create(string value)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(value);
-        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, _defaultLength);
+        // Will be disabled to make the course more easier and focus on the big picture of the microservices..
+        //ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, _defaultLength);
         return new OrderName(value);
     }
 }
