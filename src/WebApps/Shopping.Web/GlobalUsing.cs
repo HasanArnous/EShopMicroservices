a315adde8387b236a3e3381f0f522980c9175ac1
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-09-30 20:40:36 +0300

    WebApps --> Shopping.Web: Created the required models and service inteface to consume the Ordering Service.

--- src/WebApps/Shopping.Web/GlobalUsing.cs
+++ src/WebApps/Shopping.Web/GlobalUsing.cs
@@ -2,3 +2,4 @@
 global using Shopping.Web.Services;
 global using Shopping.Web.Models.Basket;
 global using Shopping.Web.Models.Catalog;
+global using Shopping.Web.Models.Ordering;
---
+++ src/WebApps/Shopping.Web/Services/IOrderingService.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.Web.Services;
+
+public interface IOrderingService
+{
+	[Get("/ordering-service/orders/")]
+	Task<GetOrdersResponse> GetOrders(int pageIndex = 0, int pageSize = 10);
+
+	[Get("/ordering-service/orders/customer/{customerid}")]
+	Task<GetOrdersByCustomerResponse> GetOrderByCustomer(Guid customerId);
+
+	[Get("/ordering-service/orders/{orderName}")]
+	Task<GetOrdersByNameResponse> GetOrderByName(string orderName);
+}
