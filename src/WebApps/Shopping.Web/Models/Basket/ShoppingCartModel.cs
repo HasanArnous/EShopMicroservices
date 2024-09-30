329c582af171a5c2f55f61fda41b594a6587c883
HasanArnous <hasan.arnous.andriome@gmail.com>
2024-09-30 20:14:11 +0300

    WebApps --> Shopping.Web: Created the required models and service inteface to consume the Basket Service.

---
+++ src/WebApps/Shopping.Web/Models/Basket/ShoppingCartModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Shopping.Web.Models.Basket;
+
+public class ShoppingCartModel
+{
+	public string Username { get; set; } = default!;
+	public List<ShoppingCartItemModel> Items { get; set; } = default!;
+	public decimal TotalPrice => Items == null
+		? 0m
+		: Items.Sum(i => i.Price * i.Quantity);
+}
+
+public class ShoppingCartItemModel
+{
+	public Guid ProductId { get; set; } = default!;
+	public string ProductName { get; set; } = default!;
+	public decimal Price { get; set; } = default!;
+	public int Quantity { get; set; } = default!;
+	public string Color { get; set; } = default!;
+}
+
+// Wrapper classes
+public record StoreBasketRequest(ShoppingCartModel ShoppingCart);
+public record StoreBasketResponse(string Username);
+public record GetBasketResponse(ShoppingCartModel ShoppingCart);
+public record DeleteBasketResponse(bool IsSuccess);
---
+++ src/WebApps/Shopping.Web/Services/IBasketService.cs
@@ -0,0 +1,17 @@
+﻿namespace Shopping.Web.Services;
+
+public interface IBasketService
+{
+	[Get("/basket-service/basket/{username}")]
+	Task<GetBasketResponse> GetBasket(string username);
+
+	[Post("/basket-service/basket/{username}")]
+	// Make sure the parameter name of the request body match the same name in the endpoint....
+	Task<StoreBasketResponse> StoreBasket(string username, StoreBasketRequest request);
+
+	[Delete("/basket-service/basket/{username}")]
+	Task<DeleteBasketResponse> DeleteBasket(string username);
+
+	[Post("/basket-service/basket/checkout")]
+	Task<CheckoutBasketResponse> CheckoutBasket(CheckoutBasketRequest request);
+}
